d756a7957b9eb7124eb7387becb499aaad372574
Nidhi Shettigar <shettigarnidhi813@gmail.com>
2022-02-14 23:57:38 +0530

    overlay

--- script.js
+++ script.js
@@ -12,7 +12,27 @@ function toggleNavbar(){
     document.querySelector(".header").classList.toggle("active");
 }
 /* active section */
-1:47:52 
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+        document.querySelector(".overlay").classList.add("active");
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")){
+            toggleNavbar();
+        }
+        else{
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(() => {
+            document.querySelector("section.active").classList.remove("active","fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.remove("hide-scrolling");
+            navToggler.classList.remove("hide");
+            document.querySelector(".overlay").classList.remove("active");
+        }, 500);
+    }
+});
 /*  about tabs   */
 const tabsContainer = document.querySelector(".about-tabs"),
 aboutSection = document.querySelector(".about-section");
